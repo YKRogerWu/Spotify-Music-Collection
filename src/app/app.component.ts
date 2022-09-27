78c6a4b4c44bb67ddb88fe901b6cdd51adf25cd4
Roger Wu <95989448+YKRogerWu@users.noreply.github.com>
2022-09-27 13:36:09 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,3 @@
-/*********************************************************************************
- * WEB422 – Assignment 06
- * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
- * No part of this assignment has been copied manually or electronically from any other source
- * (including web sites) or * distributed to other students.
- *
- * Name: Roger Wu | Student ID: 146740204 | Date: 2022-08-07
- * Angular App (Deployed) Link: 
- * User API (Heroku) Link: https://morning-ravine-01265.herokuapp.com/
- * ********************************************************************************/
 import { Component } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
 import { AuthService } from './auth.service';
